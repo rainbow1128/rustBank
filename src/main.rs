886b7200e01d5da886d4221fe3efcea350d8d288
rainbow1128 <peterjeffreyvr38@gmail.com>
2023-12-28 00:25:29 +0530

    added connnections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Conncection Establised");
+            }
+            Err(e) => {
+                eprintln!("Error while listening to the server :{}", e)
+            }
+        }
+    }
 }
